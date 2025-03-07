40e6967f9f3feba7fedddf7f7f4fa070c575c5b6
Kasssiiii <kasia.gru@gmail.com>
2025-03-07 01:36:46 +0100

    added JS for user's selection including conditional statements to display a message to the user based on the input

---
+++ script.js
@@ -0,0 +1,29 @@
+const allFilter = document.getElementById("all-filter");
+allFilter.onclick = () => {
+  clickButton(allFilter)
+};
+const italyFilter = document.getElementById("italy-filter");
+italyFilter.onclick = () => {
+  clickButton(italyFilter)
+};
+const usaFilter = document.getElementById("usa-filter");
+usaFilter.onclick = () => {
+  clickButton(usaFilter)
+};
+const chinaFilter = document.getElementById("china-filter");
+chinaFilter.onclick = () => {
+  clickButton(chinaFilter)
+};
+
+const clickButton = (button) => {
+    const text = button.textContent;
+    if (text == "USA") {
+        alert("You have chosen american cuisine");
+    } else if (text == "China") {
+        alert("You have chosen chineese cuisine");
+    } else if (text == "Italy") {
+        alert("You have chosen italian cuisine");
+    } else {
+        alert("You have chosen recipies from all cuisines");
+    }
+};
